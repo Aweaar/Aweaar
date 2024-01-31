7066ee07d9806801baea871477702d8767aae50c
Aweaar <114052180+Aweaar@users.noreply.github.com>
2024-02-01 01:13:51 +0300

    C02

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaozakca <kaozaca@student.42kocaeli.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 22:00:04 by kaozakca          #+#    #+#             */
+/*   Updated: 2024/01/31 22:01:21 by kaozakca         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaozakca <kaozaca@student.42kocaeli.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 22:32:29 by kaozakca          #+#    #+#             */
+/*   Updated: 2024/01/31 22:39:00 by kaozakca         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char	*str)
+{
+	int	i;
+	int	i1;
+
+	i = 0;
+	i1 = 1;
+	ft_lowercase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i1 == 1)
+				str[i] -= 32;
+			i1 = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			i1 = 0;
+		else
+			i1 = 1;
+		i++;
+	}
+	return (str);
+}
